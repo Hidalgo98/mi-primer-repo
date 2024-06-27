b4008dc0f054914ea4be707c71bf03f710828ff5
hidalgo98 <jahidalgo19@espe.edu.ec>
2024-06-27 17:24:25 -0500

    primer commit

---
+++ mi proyecto/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Imprimir tu nombre
+        String nombre = "julio hidalgo";
+        System.out.println("Nombre: " + nombre);
+
+        // Imprimir tu edad
+        int edad = 25; // Cambia esto a tu edad real
+        System.out.println("Edad: " + edad);
+
+        // Imprimir "Hola Mundo"
+        System.out.println("Hola Mundo");
+    }
+}
